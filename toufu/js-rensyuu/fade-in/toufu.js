6885478ee49731b99414026967c1a5a4f60d0e31
takahasihashigo <haruharu@kinakokinako.com>
2022-04-23 18:54:45 +0900

    js/fade-in

--- toufu/js-rensyuu/fade-in/toufu.js
+++ toufu/js-rensyuu/fade-in/toufu.js
@@ -1,3 +1,7 @@
+/* 
+メニュー(navbar)の文字の色を変更
+この場合はgetElementByIdの方が楽だけどまあいいや
+*/
 function changeNavColor(){
     const kokodayo = document.getElementsByClassName('kokodayo');
     /* メニューの親要素(navbar-nav)を取得 */
@@ -5,7 +9,7 @@ function changeNavColor(){
     /* viewportから白い背景までの距離(オフセット)を計算 */
     const offsetKokodayo = kokodayo[0].getBoundingClientRect().top;
     /* (TEST用)オフセットの確認 */
-    console.log(offsetKokodayo);
+    console.log('kokodayo：', offsetKokodayo);
 
     /* viewportの上部まで来た時にメニューの文字の色を変更 */
     if (offsetKokodayo <= 0) {
@@ -16,4 +20,16 @@ function changeNavColor(){
     }
 }
 
+function fadeinTest() {
+    const fadeIn = document.getElementsByClassName('fadein-area')
+    const offsetFade01 = fadeIn[0].getBoundingClientRect().top;
+    /* (TEST用)オフセットの確認 */
+    console.log('fade-in：', offsetFade01)
+
+    if (offsetFade01 <= 500) {
+        fadeIn[0].classList.add('add-fadein');
+    }
+}
+
 window.addEventListener('scroll', changeNavColor);
+window.addEventListener('scroll', fadeinTest);
