1542e265f6420fa005412af23df6b10423fb5eea
takahasihashigo <haruharu@kinakokinako.com>
2022-04-23 15:56:29 +0900

    js/fade-in

---
+++ toufu/js-rensyuu/fade-in/toufu.js
@@ -0,0 +1,19 @@
+function changeNavColor(){
+    const kokodayo = document.getElementsByClassName('kokodayo');
+    /* メニューの親要素(navbar-nav)を取得 */
+    const navbarNav = document.getElementsByClassName('navbar-nav');
+    /* viewportから白い背景までの距離(オフセット)を計算 */
+    const offsetKokodayo = kokodayo[0].getBoundingClientRect().top;
+    /* (TEST用)オフセットの確認 */
+    console.log(offsetKokodayo);
+
+    /* viewportの上部まで来た時にメニューの文字の色を変更 */
+    if (offsetKokodayo <= 0) {
+        navbarNav[0].classList.add('kuro');
+    }
+    else {
+        navbarNav[0].classList.remove('kuro');
+    }
+}
+
+window.addEventListener('scroll', changeNavColor);
